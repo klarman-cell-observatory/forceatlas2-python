46aceaaee6ac32ee2dce0af98cad31360c7a818d
jgould <jgould@broadinstitute.org>
2019-08-28 11:24:33 -0400

    Removed print statements

--- forceatlas2/__init__.py
+++ forceatlas2/__init__.py
@@ -4,38 +4,36 @@ from subprocess import check_call
 
 
 def forceatlas2(file_name, graph, n_jobs, target_change_per_node, target_steps, is3d, memory, random_state, init):
-
     input_graph_file = "{file_name}.net".format(file_name=file_name)
     graph.write(input_graph_file)
-    print(input_graph_file + " is written.")
 
     output_coord_file = "{file_name}.coords.txt".format(file_name=file_name)
 
     classpath = (
-        pkg_resources.resource_filename("forceatlas2", "ext/forceatlas2.jar")
-        + ":"
-        + pkg_resources.resource_filename("forceatlas2", "ext/gephi-toolkit-0.9.2-all.jar")
+            pkg_resources.resource_filename("forceatlas2", "ext/forceatlas2.jar")
+            + ":"
+            + pkg_resources.resource_filename("forceatlas2", "ext/gephi-toolkit-0.9.2-all.jar")
     )
 
     command = [
-        "java",
-        "-Djava.awt.headless=true",
-        "-Xmx{memory}g".format(memory=memory),
-        "-cp",
-        classpath,
-        "kco.forceatlas2.Main",
-        "--input",
-        input_graph_file,
-        "--output",
-        file_name + ".coords",
-        "--nthreads",
-        str(n_jobs),
-        "--seed",
-        str(random_state),
-        "--targetChangePerNode",
-        str(target_change_per_node),
-        "--targetSteps",
-        str(target_steps),
+            "java",
+            "-Djava.awt.headless=true",
+            "-Xmx{memory}g".format(memory=memory),
+            "-cp",
+            classpath,
+            "kco.forceatlas2.Main",
+            "--input",
+            input_graph_file,
+            "--output",
+            file_name + ".coords",
+            "--nthreads",
+            str(n_jobs),
+            "--seed",
+            str(random_state),
+            "--targetChangePerNode",
+            str(target_change_per_node),
+            "--targetSteps",
+            str(target_steps),
     ]
     if not is3d:
         command.append("--2d")
@@ -54,16 +52,13 @@ def forceatlas2(file_name, graph, n_jobs, target_change_per_node, target_steps, 
         df.to_csv(init_coord_file, sep="\t", float_format="%.2f")
         command.extend(["--coords", init_coord_file])
 
-    print(command)
     check_call(command)
 
     fle_coords = pd.read_csv(output_coord_file, header=0, index_col=0, sep="\t").values
+    os.remove(input_graph_file)
+    os.remove(output_coord_file)
 
-    check_call(["rm", "-f", input_graph_file])
-    check_call(["rm", "-f", output_coord_file])
     if init is not None:
         os.remove("{file_name}.init.coords.txt".format(file_name=file_name))
-
-    print("Force-directed layout is generated.")
 
     return fle_coords
