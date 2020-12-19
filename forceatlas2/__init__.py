2a9a76c3cc6a5d9aa14b143932316f4eee26b5b9
Yiming Yang <yang.yihming@gmail.com>
2020-12-19 02:42:45 -0500

    fix bug

--- forceatlas2/__init__.py
+++ forceatlas2/__init__.py
@@ -8,7 +8,7 @@ except ImportError:  # < Python 3.8: Use backport module
     from importlib_metadata import version, PackageNotFoundError
 
 try:
-    __version__ = version(__name__)
+    __version__ = version('forceatlas2-python')
     del version
 except PackageNotFoundError:
     pass
